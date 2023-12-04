c37b1a62aada6215303b0a56399e284f2f87a04f
Venum693 <venum693@gmail.com>
2023-12-04 14:02:36 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/flutter_mapbox/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.flutter_mapbox;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodChannel;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class MainActivity extends FlutterActivity {
+    private static final String CHANNEL = "navigation_channel";
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                        (call, result) -> {
+                            if (call.method.equals("launchMapScreen")) {
+                                launchMapScreen();
+                            } else {
+                                result.notImplemented();
+                            }
+                        }
+                );
+    }
+
+    private void launchMapScreen() {
+        Intent intent = new Intent(this, YourNewActivity.class);
+        intent.putExtra("textToShow", "hi");
+        startActivity(intent);
+    }
+}
---
+++ lib/mapbox_through_channel.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class NavgationFromAndroid extends StatefulWidget {
+  const NavgationFromAndroid({super.key});
+
+
+  @override
+  State<NavgationFromAndroid> createState() => _NavgationFromAndroidState();
+}
+
+class _NavgationFromAndroidState extends State<NavgationFromAndroid> {
+
+  static const platform = MethodChannel('navigation_channel');
+
+  Future<void> launchMapScreen() async {
+    try {
+      await platform.invokeMethod('launchMapScreen');
+    } on PlatformException catch (e) {
+      print("Error: ${e.message}");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            launchMapScreen();
+          },
+          child: Text("Press"),
+        ),
+      ),
+    );
+  }
+}
